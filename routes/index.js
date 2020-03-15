393bd56873d751f9a719a29254f6d59c6342accc
Naser <nasersoftcorporation2013@gmail.com>
2020-03-15 17:53:21 +0600

    Fully functional add and update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const { addUser, updateUser } = require('./users');
 
 router.get('/', (req, res) => {
   res.status(200).json({ success: true, message: 'Index route' });
 });
+
+// Add user
+router.get('/add', addUser);
+
+// Update user
+router.put('/update', updateUser);
 
 module.exports = router;
